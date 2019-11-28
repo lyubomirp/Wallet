c4cb01a6ac851de09964e3520224530c071f0abc
lyubomirp <lpetkov44@yahoo.com>
2019-11-28 15:50:57 +0200

    Fixed rules and added input sanitization

--- app/Http/Requests/DepositRequest.php
+++ app/Http/Requests/DepositRequest.php
@@ -23,10 +23,27 @@ class DepositRequest extends FormRequest
      */
     public function rules()
     {
+        $this->array_strip_tags(parent::all());
         return [
-            'requested_amount'=>'required|between:0,9999999999.99',
-            'currency'=>'required|min:1|max:3',
-            'commission_fee'=>'required'
+            'requested_amount'=>'required|numeric|between:0,9999999999.99',
+            'currency'=>'required|in:eur,usd,gbp|min:1|max:3',
+            'commission_fee'=>'required|numeric'
         ];
+    }
+
+    public function array_strip_tags(Array $array)
+    {
+        $result = array();
+
+        foreach ($array as $key => $value) {
+            $key = strip_tags($key);
+            if (is_array($value)) {
+                $result[$key] = static::array_strip_tags($value);
+            } else {
+                $result[$key] = trim(strip_tags($value));
+            }
+        }
+
+        return $result;
     }
 }
